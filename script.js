21880ac88146e025c235906639e029163478bb51
HOMEPC\Del <delmessmith@hotmail.com>
2016-10-25 20:55:54 -0400

    -Changed Gas Pump image -Created a function to shorten getElementById -Created function to enable text boxes when check boxes are checked and disable/clear them when unchecked -Added onclick handlers for each checkbox that call my isChecked function -Declared some variables for my different categories

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
 /**
  * Created by Del on 10/24/2016.
  */
+var Rent = 0;
+var Mortgage = 0;
+var Water = 0;
+var Gas = 0;
+var Electric = 0;
+var TV = 0;
+var Internet = 0;
+var CellPhone = 0;
+var HomePhone = 0;
+var Pet = 0;
+var CreditCard = 0;
+var Groceries = 0;
+var Restaurants = 0;
+var Entertainment = 0;
+var Savings = 0;
+var Investments = 0;
+var Other = 0;
+
+var CategoriesTotal = 5;
+var ContTotal = 4;
+
+
+function $(element) {
+   return document.getElementById(element);
+}
+
+function isChecked(Category) {
+    if ($('chk' + Category).checked == true){
+        $('txt' + Category).disabled = false;
+    } else {
+        $('txt' + Category).value = "";
+        $('txt' + Category).disabled = true;
+    }
+}
+
+function Calculate() {
+    ValidateInput();
+
+    DisplayLowFundsMsg();
+}
+
+function GetInput() {
+    
+}
+
+function ValidateInput(Category) {
+    GetInput(Category);
+    
+    if (parseFloat("txt" + Category) >= 0){
+        
+    }
+        
+    else {alert("Please Enter a Valid, Positive Number")}
+}
+
+function DisplayLowFundsMsg() {
+    if (CategoriesTotal > ContTotal){
+        $("divLowFunds").innerHTML = "You are over budget!"
+    } else $("divLowFunds").innerHTML = "";
+}
